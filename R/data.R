9227fbd54892e8271972df98d6d7239d706c6d91
Diego Diez <diego10ruiz@gmail.com>
2016-12-03 12:06:31 +0900

    Initial commit.

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Expression data fro immune cells.
+#'
+#' An ExpressionSet object containing expression levels for 217 immune cells.
+#'
+#' @format An ExpressionSet with 22135 features and 778 samples.
+#' @source \url{http://www.immgen.org}
+"immgen"
---
+++ R/help.R
@@ -0,0 +1,8 @@
+#' A Package with Data from the Immgen Project
+#'
+#' This package collects R/Bioconductor objects with experimental data
+#' from the Immunological Genome Project (ImmGen; http://immgen.org).
+#'
+#' @name immgen.db-package
+#' @docType package
+NULL
